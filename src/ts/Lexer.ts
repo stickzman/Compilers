a1a4f6afb5bf9dead6056adb0ec02baed66de0f8
stickzman <Daniel.ahl1@marist.edu>
2018-01-20 19:19:18 -0500

    Lexer ignores whitespace now. Added Log statements

--- src/ts/Lexer.ts
+++ src/ts/Lexer.ts
@@ -2,6 +2,9 @@ function tokenizeInput() {
   let sourceElem = <HTMLInputElement>document.getElementById("source");
   let source = sourceElem.value;
 
+  Log.level = LogPri.VERBOSE;
+  Log.clear();
+
   //Begin generating tokens from source code
   getTokens(source);
 }
@@ -13,23 +16,28 @@ function getTokens(source: string): Token[] {
   switch (source.charAt(0)) {
     case '{':
       token = new Token("LBRACE");
+      Log.print("LEXER: '{'	-->	[LBRACE]");
       //Get the rest of the tokens recursively
       otherTokens = getTokens(source.substring(1));
       return [token].concat(otherTokens);
     case '}':
       token = new Token("RBRACE");
+      Log.print("LEXER: '}'	-->	[RBRACE]");
       otherTokens = getTokens(source.substring(1));
       return [token].concat(otherTokens);
     case '(':
       token = new Token("LPAREN");
+      Log.print("LEXER: '('	-->	[LPAREN]");
       otherTokens = getTokens(source.substring(1));
       return [token].concat(otherTokens);
     case ')':
       token = new Token("RPAREN");
+      Log.print("LEXER: ')'	-->	[RPAREN]");
       otherTokens = getTokens(source.substring(1));
       return [token].concat(otherTokens);
     case '$':
       token = new Token("EOP");
+      Log.print("LEXER: '$'	-->	[EOP]");
       otherTokens = getTokens(source.substring(1));
       return [token].concat(otherTokens);
     case '/':
@@ -48,7 +56,18 @@ function getTokens(source: string): Token[] {
         }
         return getTokens(source.substring(i));
       }
+    case ' ':
+      //Skip whitespace
+      return getTokens(source.substring(1));
+    case '\t':
+      //Skip whitespace
+      return getTokens(source.substring(1));
+    case '\n':
+      //Skip whitespace
+      return getTokens(source.substring(1));
     default:
+      Log.print("LEXER: ERROR: Unidentified token '"
+                  + source.charAt(0) + "' encountered");
       return getTokens(source.substring(1));
   }
 }
