e239538595ee3324bd81361062a63cc509d7d656
stickzman <Daniel.ahl1@marist.edu>
2018-01-23 16:20:17 -0500

    Fixed comment col tracking bug

--- src/ts/Lexer.ts
+++ src/ts/Lexer.ts
@@ -18,7 +18,7 @@ function tokenizeInput() {
   function getTokens(source: string, last?: Token): Token {
     if (source.length <= 0) {
       if (last === undefined || last.name === "EOP") {
-        return;
+        return null;
       }
       numWarns++;
       Log.print("LEXER: WARNING: Missing EOP character '$' on line " + lineNum
@@ -31,64 +31,89 @@ function tokenizeInput() {
     //Look for all multi-character tokens using RegExp
     if (/^print/.test(source)) {
       token = createToken("print", "PRINT", last);
+      charNum += 5;
       getTokens(source.substring(5), token);
     } else if (/^while/.test(source)) {
       token = createToken("while", "WHILE", last);
+      charNum += 5;
       getTokens(source.substring(5), token);
     } else if (/^if/.test(source)) {
       token = createToken("if", "IF", last);
+      charNum += 2;
       getTokens(source.substring(2), token);
     } else if (/^int/.test(source)) {
       token = createToken("int", "INT", last);
+      charNum += 3;
       getTokens(source.substring(3), token);
     } else if (/^string/.test(source)) {
       token = createToken("string", "STRING", last);
+      charNum += 6;
       getTokens(source.substring(6), token);
     } else if (/^boolean/.test(source)) {
       token = createToken("boolean", "BOOLEAN", last);
+      charNum += 7;
       getTokens(source.substring(7), token);
     } else if (/^false/.test(source)) {
       token = createToken("false", "FALSE", last);
+      charNum += 5;
       getTokens(source.substring(5), token);
     } else if (/^true/.test(source)) {
       token = createToken("true", "TRUE", last);
+      charNum += 4;
       getTokens(source.substring(4), token);
     } else if (/^==/.test(source)) {
       token = createToken("==", "EQUAL", last);
+      charNum += 2;
       getTokens(source.substring(2), token);
     } else if (/^!=/.test(source)) {
       token = createToken("!=", "NOTEQUAL", last);
+      charNum += 2;
       getTokens(source.substring(2), token);
     } else if (/^\/\*/.test(source)) {
       //Skip to end of comment
       let index = source.indexOf("*/");
+      let cmt: string;
       if (index === -1) {
         //Reached end of file
-
-        //Add number of lines in comment to total lineNum
-        lineNum += source.split('\n').length-1;
-
         numWarns++;
         Log.print("LEXER: WARNING: Unclosed comment block on line " + lineNum,
           LogPri.WARNING);
-        return getTokens(source.substring(source.length), last);
+
+        cmt = source;
       } else {
-        //Add number of lines in comment to total lineNum
-        lineNum += source.substr(0, index+2).split('\n').length-1;
-        return getTokens(source.substring(index+2));
+        cmt = source.substr(0, index+2);
+      }
+      let numLines = cmt.split('\n').length-1; //Number of lines in the comment
+      //Add number of lines in comment to total lineNum
+      lineNum += numLines;
+      let newLineIndex = cmt.lastIndexOf('\n');
+      if (newLineIndex !== -1) {
+        //Set charNum to the number of characters on the last line (including "*/")
+        charNum = cmt.length - newLineIndex;
+      } else {
+        //If the comment is one line, add it to charNum
+        charNum += cmt.length;
+      }
+      if (index === -1) {
+        token = getTokens(source.substring(source.length), last);
+      } else {
+        token = getTokens(source.substring(cmt.length), last);
       }
     } else if (/^\*\//.test(source)) {
       Log.print("LEXER: ERROR: Unmatched '*/' encountered on line " + lineNum
         + ". Did you mean '/*'?", LogPri.ERROR);
+      charNum += 2;
       getTokens(source.substring(2), last);
     } else if (/^[a-z]/.test(source)) {
       //The first character is a lowercase letter
       token = createToken(source.charAt(0), "ID", last, source.charAt(0));
+      charNum += 1;
       getTokens(source.substring(1), token);
     } else if (/^[0-9]/.test(source)) {
       //The first character is a digit
       let num = Number(source.charAt(0));
       token = createToken(source.charAt(0), "DIGIT", last, num);
+      charNum += 1;
       getTokens(source.substring(1), token);
     }
 
@@ -100,22 +125,27 @@ function tokenizeInput() {
       case '{':
         token = createToken("{", "LBRACE", last);
         //Get the rest of the tokens recursively
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case '}':
         token = createToken("}", "RBRACE", last);
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case '(':
         token = createToken("(", "LPAREN", last);
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case ')':
         token = createToken(")", "RPAREN", last);
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case '$':
         token = createToken("$", "EOP", last);
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case '"':
@@ -134,32 +164,40 @@ function tokenizeInput() {
         if (/^[a-z" ]*$/.test(str)) {
           //The string literal contains only valid characters
           token = createToken(str, "STRLIT", last, str);
+          charNum += str.length;
           getTokens(source.substring(index+1), token);
+          return token;
         } else {
           //The string contains invalid charaters
           numErrors++;
           Log.print("LEXER: ERROR: String literal '" + str +
-            "' on line " + lineNum + " contains invalid characters. " +
+            "' at line:" + lineNum + " col:" + charNum +
+            " contains invalid characters. " +
             "Strings can contain lowercase letters and spaces.", LogPri.ERROR);
+          charNum += str.length;
           return getTokens(source.substring(index+1), token);
         }
-        return token;
       case '+':
         token = createToken("+", "INTOP", last);
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case '=':
         token = createToken("=", "ASSIGN", last);
+        charNum += 1;
         getTokens(source.substring(1), token);
         return token;
       case ' ':
         //Skip whitespace
+        charNum += 1;
         return getTokens(source.substring(1), last);
       case '\s':
         //Skip whitespace
+        charNum += 1;
         return getTokens(source.substring(1), last);
       case '\t':
         //Skip whitespace
+        charNum += 1;
         return getTokens(source.substring(1), last);
       case '\n':
         lineNum++;
@@ -190,7 +228,6 @@ function tokenizeInput() {
       last.next = token;
     }
     Log.print(`LEXER: '${chars}' --> [${name}] at line:${lineNum} col:${charNum}`);
-    charNum += chars.length;
     return token;
   }
 
