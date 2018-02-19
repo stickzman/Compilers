8138ebdf3f671d995c0c2f577f72cc1b5449ed31
stickzman <Daniel.ahl1@marist.edu>
2018-02-19 01:53:55 -0500

    Began coding parser

--- bin/compiler.js
+++ bin/compiler.js
@@ -370,8 +370,33 @@ class Log {
 Log.level = LogPri.VERBOSE;
 function parse(token) {
     let root = new TNode("Program");
+    parseBlock(root);
+    match(["$"], root);
+    function parseBlock(parent) {
+        let blockNode = new TNode("Block");
+        parent.addChild(blockNode);
+        match(["{"], blockNode);
+        parseStatementList(blockNode);
+        match(["}"], blockNode);
+    }
     function nextToken() {
         token = token.next;
+    }
+    //Matches list of tokens by characters
+    //formatted as an array of strings.
+    //Prints error if match not found.
+    function match(tList, parent) {
+        for (let char of tList) {
+            if (char === token.char) {
+                parent.addChild(new TNode(token.name));
+                nextToken();
+            }
+            else {
+                Log.print(`Error: Expected ${char} found ${token.char}` +
+                    ` at line: ${token.line} col: ${token.col}.`);
+                return;
+            }
+        }
     }
 }
 class Token {
--- src/ts/Parser.ts
+++ src/ts/Parser.ts
@@ -1,10 +1,34 @@
 function parse(token: Token) {
 
   let root = new TNode("Program");
+  parseBlock(root);
+  match(["$"], root);
 
+  function parseBlock(parent: TNode) {
+    let blockNode = new TNode("Block");
+    parent.addChild(blockNode);
+    match(["{"], blockNode);
+    parseStatementList(blockNode);
+    match(["}"], blockNode);
+  }
 
   function nextToken() {
     token = token.next;
   }
 
+  //Matches list of tokens by characters
+  //formatted as an array of strings.
+  //Prints error if match not found.
+  function match(tList: string[], parent: TNode) {
+    for (let char of tList) {
+      if (char === token.char) {
+        parent.addChild(new TNode(token.name));
+        nextToken();
+      } else {
+        Log.print(`Error: Expected ${char} found ${token.char}` +
+          ` at line: ${token.line} col: ${token.col}.`);
+        return;
+      }
+    }
+  }
 }
